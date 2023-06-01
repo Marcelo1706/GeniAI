b4fd6cfd224a5cf2ea0cbc75d84707bbaf2a5d2a
Jose  Cartagena <jcartagena@applaudostudios.com>
2023-06-01 15:19:40 -0600

    C.A.2 Back-end logic to handle user interactions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import re
+import json
 #to read .env-----------------------------------------------------------------------------
 from pathlib import Path 
 from dotenv import load_dotenv
@@ -18,7 +19,7 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 def message_any(message, say, client):
     # Get the user ID
     user_id = message['user']
-    mx =message
+   
     # Retrieve the user's information
     user_info = client.users_info(user=user_id)
     
@@ -28,7 +29,32 @@ def message_any(message, say, client):
     # Send a response message with the user's email
     if email:
         #code to validate permissions and roles
-        say(f"Your email is: {email}")
+            #say(f"Your email is: {email}", channel=user_id)
+
+        messageFromUser = message["text"].split() 
+        print(f"Message from User:  {messageFromUser}")
+        print(len(messageFromUser))
+
+        #CA2 API call to handle "Company name" and return general info      #company_name  
+        companyFormat = r"^#[a-zA-Z0-9_]+$"
+        #CA2 API call to handle "Company name" and return general info with Email  #company_name  +email
+        companyEmailFormat = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b" 
+                            
+       
+        if len(messageFromUser)<3 and re.match(companyFormat, messageFromUser[1]):#Returns info to slack
+            #say(f"Your asking for company{message} info")
+            print(f"Your asking for  {messageFromUser[1]} ")
+        elif (len(messageFromUser)==3) and (re.match(companyFormat, messageFromUser[1])) and  ("mailto:" in  messageFromUser[2]):#Retursn Info to Slack and send info Users Email 
+
+            #say(f"Your asking for company{message} info")
+            print(f"Your asking for  {messageFromUser[1]} info and send it to {email} account")
+        else:
+            #say(f"Sorry, I can not understand that instruction")
+            print("Sorry, I can not understand that instruction")
+        
+           
+
+        
     else:
         say("Sorry, I couldn't retrieve your email to validate your permissions.")
 
@@ -40,4 +66,3 @@ def message_any(message, say, client):
 # Start app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-    
